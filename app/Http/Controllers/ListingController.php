1efd97636604c3f50887bc24dd31b13c6cbffd76
Mohamed-Mamdouh-0 <mohamed.mamdouh.ss95@gmail.com>
2023-06-25 18:59:59 +0300

    adding controllers

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+
+class ListingController extends Controller
+{
+    //show all listings
+    public function index(){
+        return view('listings',['listings' => Listing::all()]);
+    }
+    // show single list
+    public function show(Listing $listing){
+        return view('listing',['listing'=> $listing]);
+    }
+}
